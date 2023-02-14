2c426cc7896ba39560170a16854a23a7323883ec
Duarte Pires Lopes <49475154+Jefex7@users.noreply.github.com>
2023-02-14 08:43:55 +0000

    Update src/service/serviceStatus.js
    Co-authored-by: MUSTAFA <mdayican@gmail.com>

--- src/service/serviceStatus.js
+++ src/service/serviceStatus.js
@@ -19,7 +19,7 @@ function getCcdOrchestratorService(env) {
 if(env === 'demo' || env === 'ithc'){
     return new Service('em-ccdorc', `http://em-ccdorc-${env}.service.core-compute-${env}.internal`);
   }else{
-    return new Service('em-ccd-orchestrator', `http://em-ccd-orchestrator-${env}.service.core-compute-${env}.internal`)
+    return new Service('em-ccd-orchestrator', `http://em-ccd-orchestrator-${env}.service.core-compute-${env}.internal`);
   }
 }
 
