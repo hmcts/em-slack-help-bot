177ded33254ea43d2a6df17d6e55bda7e3cb0312
lars lunde <lars.lunde@hmcts.net>
2024-01-11 11:36:22 +0000

    added adoption team to dropdown

--- src/messages.js
+++ src/messages.js
@@ -407,6 +407,7 @@ function openHelpRequestBlocks() {
                     },
                     "options": [
                         option('Access Management', 'am'),
+                        option('Adoption'),
                         option('Architecture'),
                         option('Bulk scan', 'bulkscan'),
                         option('Bulk print', 'bulkprint'),
